2386a4ae955a6d3e19975b11b1f065855d2c5523
morning120429 <forever120429@gmail.com>
2024-07-08 15:57:21 -0400

    update some fields

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, request, jsonify
 import json
 from docx import Document
 from docx.shared import Inches
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-document_name = os.getenv("DOCUMENT_NAME")
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Zapier Webhook URL : https://skilled-albacore-indirectly.ngrok-free.app
+
+counter = 1
 
 @app.route('/zap-webhook', methods=['POST'])
 def webhook():
     data = request.json
     if data:
+        global counter
         # Process the received data
         print(f'Received data: {data}')
 
@@ -30,6 +30,9 @@ def webhook():
             "status": "success",
             "message": "Data received successfully!"
         }
+
+        counter += 1
+
         return jsonify(response), 200
     else:
         return jsonify({"status": "failure", "message": "No data received"}), 400
@@ -52,22 +55,13 @@ def extract_data(text):
         emergency_phone = splited_texts[11].split(": ")[1]
         case_state = splited_texts[12].split(": ")[1]
         case_type = splited_texts[13].split(": ")[1]
-        state = splited_texts[4].split(": ")[1]
-        zip = splited_texts[5].split(": ")[1]
-        home_phone = splited_texts[6].split(": ")[1]
-        cell_phone = splited_texts[7].split(": ")[1]
-        email = splited_texts[8].split(": ")[1]
-        agreed = splited_texts[9]
-        emergency_name = splited_texts[10].split(": ")[1]
-        emergency_phone = splited_texts[11].split(": ")[1]
-        case_state = splited_texts[12].split(": ")[1]
-        case_type = splited_texts[13].split(": ")[1]
         firm_name = splited_texts[14].split(": ")[1]
         attorney_name = splited_texts[15].split(": ")[1]
         attorney_phone = splited_texts[16].split(": ")[1]
-        amount = splited_texts[17].split(": ")[1]
-        payment_method = splited_texts[18].split(": ")[1]
-        description = splited_texts[19].split(": ")[1]
+        attorney_email = splited_texts[17].split(": ")[1]
+        amount = splited_texts[18].split(": ")[1]
+        payment_method = splited_texts[19].split(": ")[1]
+        description = splited_texts[20].split(": ")[1]
 
         final_data.append({
             "first_name": first_name,
@@ -87,6 +81,7 @@ def extract_data(text):
             "firm_name": firm_name,
             "attorney_name": attorney_name,
             "attorney_phone": attorney_phone,
+            "attorney_email": attorney_email,
             "amount": amount,
             "payment_method": payment_method,
             "description": description
@@ -121,34 +116,37 @@ def find_data(json_data):
     attorney_name = json_data['attorney_name']
     print(f'<Lawyer_Name> ---> {attorney_name}')
     
-    firm_address = ""
+    firm_address = json_data['attorney_email']
     print(f'<Lawyer_Email> ---> {firm_address}')
 
     loan_date = ""
     print(f'<Loan_Date> ---> {loan_date}')
 
-    case_id = ""
+    today = datetime.today()
+    date_str = today.strftime("%Y-%m-%d")
+    today_as_date = datetime.strptime(date_str, "%Y-%m-%d")
+    case_id = today_as_date.strftime("%m%d%y") + str(f"{counter:02}")
     print(f'<Case_ID> ---> {case_id}')
 
     loan_amount = json_data['amount']
     print(f'<Loan_Amount> ---> {loan_amount}')
 
-    handling_fee = ""
+    handling_fee = "0"
     print(f'<Handling_Fee> ---> {handling_fee}')
 
     client_phone = json_data['home_phone']
     print(f'<Client_Phone> ---> {client_phone}')
 
-    q1_interest = ""
+    q1_interest = int(int(json_data['amount']) * 1.15)
     print(f'<Q1_Interest> ---> {q1_interest}')
 
-    q2_interest = ""
+    q2_interest = int(int(json_data['amount']) * 1.3)
     print(f'<Q2_Interest> ---> {q2_interest}')
 
-    q3_interest = ""
+    q3_interest = int(int(json_data['amount']) * 1.45)
     print(f'<Q3_Interest> ---> {q3_interest}')
 
-    q4_interest = ""
+    q4_interest = int(int(json_data['amount']) * 1.6)
     print(f'<Q4_Interest> ---> {q4_interest}')
 
     client_street = json_data['address']
@@ -168,10 +166,10 @@ def find_data(json_data):
         "<Loan_Amount>" : loan_amount,
         "<Handling_Fee>" : handling_fee,
         "<Client_Phone>" : client_phone,
-        "<Q1_Interest>" : q1_interest,
-        "<Q2_Interest>" : q2_interest,
-        "<Q3_Interest>" : q3_interest,
-        "<Q4_Interest>" : q4_interest,
+        "<Q1_Interest>" : str(q1_interest),
+        "<Q2_Interest>" : str(q2_interest),
+        "<Q3_Interest>" : str(q3_interest),
+        "<Q4_Interest>" : str(q4_interest),
         "<Client_Street>" : client_street,
         "<Client_CityState>" : client_citystate
     })
@@ -180,13 +178,12 @@ def find_data(json_data):
 
 def replace_data(personal_data):
     # Purchase Agreement.docx
-    doc = Document(document_name)
+    doc = Document('Purchase Agreement.docx')
 
     for key, value in personal_data[0].items():
         replace_word_in_paragraphs(doc.paragraphs, key, value)
         replace_word_in_tables(doc.tables, key, value)
-        print(f"Replace {key} to {value}")
-
+    
     doc.save(f"{personal_data[0]["<Client_Name>"]}.docx")
     print(f"Words replaced and saved to {personal_data[0]["<Client_Name>"]}.docx")
 
